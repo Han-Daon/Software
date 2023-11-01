44b867fa802990c37338da2219d9198e4c9c2556
eumjeongseok <133610014+eumjeongseok@users.noreply.github.com>
2023-11-01 20:29:51 +0900

    소프트웨어공학 계산기 코드
    시시콜콜한 출력 부분과 테스트 케이스 5번째 것을 수정하면 됩니다.
    실수형 계산이 안 되도록 downgrade(?)해주세요~

---
+++ #Console User Interface-Based Calculator.py
@@ -0,0 +1,91 @@
+#Console User Interface-Based Calculator
+
+import os
+
+def clear_console():
+    os.system('clear' if os.name == 'posix' else 'cls')
+
+def main():
+    expression = ""
+    error_flag = False  # 에러 플래그 추가
+
+    while True:
+        clear_console()
+        print("간단한 CUI 계산기")
+        print("사용 가능한 연산: +, -, *(곱셈)")
+        print("종료하려면 'q'를 입력하세요.")
+        print("=" * 20)
+        print("현재 수식:", expression)
+        print("=" * 20)
+
+        user_input = input("연산자나 피연산자를 입력하세요: ")
+        
+        if user_input == 'q':
+            break
+        elif user_input in ['+', '-', '*']:
+            expression += user_input
+            error_flag = False  # 연산자 입력 시 에러 플래그 초기화
+        elif user_input == '=':
+            if error_flag:
+                print("ERROR!")
+                input("아무 키나 누르면 계속하세요...")
+                expression = ""
+            else:
+                try:
+                    result = eval(expression)
+                    print(f"= {result}")
+                    input("아무 키나 누르면 계속하세요...")
+                    expression = ""
+                except:
+                    print("ERROR!")
+                    input("아무 키나 누르면 계속하세요...")
+                    expression = "" #예외 처리?
+        elif user_input.isdigit(): 
+            expression += user_input
+            if expression == "7503":
+                print("=> Software Interaction Lab.")
+                input("아무 키나 누르면 계속하세요...")
+                expression = ""
+        else:
+            error_flag = True  # '/' 입력 시 에러 플래그 설정
+            expression += user_input
+
+if __name__ == "__main__":
+    main()
+
+
+# 테스트 케이스 1
+# 15
+# -
+# 3
+# -
+# 10
+# = 2
+
+# 테스트 케이스 2
+# 3
+# *
+# 1
+# *
+# 5
+# = 15
+
+# 테스트 케이스 3
+# 3
+# *
+# 1
+# /
+# 5
+# = ERROR!
+
+# 테스트 케이스 4
+# 3
+# /
+# 5
+# = ERROR!
+
+# 테스트 케이스 5
+# 5.5
+# *
+# 2
+# = ERROR!
