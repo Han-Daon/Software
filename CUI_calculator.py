16487472044c87b9994ca8ad3e84a2e33b1d631e
Ymo0418 <149486725+Ymo0418@users.noreply.github.com>
2023-11-15 22:18:53 +0900

    Update CUI_calculator.py

--- CUI_calculator.py
+++ CUI_calculator.py
@@ -1,84 +1,33 @@
-#Console User Interface-Based Calculator
-
+# Console User Interface-Based Calculator
 import os
-from calculator import *
-from error_detector import *
+from enum import Enum
+import msvcrt
 
-def is_exit(var):
-    if var == 'q':
-        exit()
-    else:
-        return var
+# 이스터에그
+egg = {"100":"백점!", "10000":"만세!!!"}
 
-def clear_console():
-    os.system('clear' if os.name == 'posix' else 'cls')
-
-def main():
-    result = 0
-    operand = None
-    operator = Operator.addition
-    
-    clear_console()
-    print("간단한 CUI 계산기")
-    print("사용 가능한 연산: +, -, *(곱셈)")
-    print("종료하려면 'q'를 입력하세요.")
-
-    user_input = Operator.none  #피연산자 먼저 입력받게 함
-
-    while True:
-      if(type(user_input) == Operator):   #이전 루프에서 연산자를 입력받음 혹은 잘못된 피연산자를 입력받음
-        user_input = is_exit(input("피연산자를 입력하세요: "))
-        operand = user_input
-      else:                               #이전 루프에서 피연산자를 입력받음 혹은 잘못된 연산자를 입력받음
-        user_input = operator_kind(is_exit(input("연산자를 입력하세요: ")))
-        operator = user_input
-        if (operator == Operator.equal):
-          print(str(result))
-          user_input = operand   #등호인 경우 다음 루프에서 연산자를 입력받음
-          continue
-      
-      error = is_error(operator, result, operand)
-
-      if(error == Error.operand):
-        user_input = operator   #다음 루프에서 피연산자를 입력받음
-      elif(error == Error.operator):
-        user_input = operand    #다음 루프에서 연산자를 입력받음
-      elif(type(user_input) != Operator):
-        result = calculate(result, operator, operand)
-
-if __name__ == "__main__":
-    main()
-
-'''
-합본
-#Console User Interface-Based Calculator
-
-import os
-from enum import Enum #os 모듈과 Enum 클래스를 불러옵니다. os는 운영 체제와 상호 작용할 수 있게 해주며, Enum은 열거형(Enumerated Type)을 정의하는 데 사용됩니다.
-
-#사용자가 'q'를 입력하면 프로그램을 종료하고, 그렇지 않으면 입력 값을 반환.
-def is_exit(var): 
-    if var == 'q':
-        exit()
-    else:
-        return var
-
-#계산기에서 사용할 연산자들을 열거형으로 정의
+# 계산기에서 사용할 연산자들을 열거형으로 정의
 class Operator(Enum):
     none = 0
     addition = 1
     subtraction = 2
     multiplication = 3
     equal = 4
+    egg = 5
 
-#오류 유형을 열거형으로 정의
-class Error(Enum):
-    none = 0
-    operator = 1
-    operand = 2
+# 피연산자가 올바른지 검사합니다. 피연산자가 음수일 경우 '-'(음수)을 제외하고 검사
+def is_right_operand(operand):
+    index = 0
+    if operand[0] == '-':   #음수인 경우
+        index = 1
+    for i in range(index, len(operand)):
+        if not 48 <= ord(operand[index]) <= 57:
+            return False
+        index = index + 1
+    return operand
 
-#입력받은 문자열 연산자를 해당하는 Operator 열거형으로 변환
-def operator_kind(operator):
+# 입력받은 문자열 연산자를 해당하는 Operator 열거형으로 변환
+def is_right_operator(operator):
     if operator == '+':
         return Operator.addition
     elif operator == '-':
@@ -90,7 +39,7 @@ def operator_kind(operator):
     else:
         return False
 
-#두 피연산자와 연산자를 받아, 해당 연산을 수행하고 결과를 반환
+# 두 피연산자와 연산자를 받아, 해당 연산을 수행하고 결과를 반환
 def calculate(operand1, operator, operand2):
     if operator == Operator.addition:
         return int(operand1) + int(operand2)
@@ -98,70 +47,76 @@ def calculate(operand1, operator, operand2):
         return int(operand1) - int(operand2)
     elif operator == Operator.multiplication:
         return int(operand1) * int(operand2)
-    elif operator == Operator.none:
+    elif operator == Operator.none or operand2 == None:
         return int(operand1)
 
-#연산자와 피연산자들을 검사하여 오류가 있으면 해당 오류 유형을 반환하고, 없으면 Error.none을 반환
-def is_error(operator, operand1, operand2):
-    if not operator:
-      print("잘못된 연산자입니다.")
-      return Error.operator
-    if not is_right_operand(str(operand1)) or not is_right_operand(str(operand2)):
-      print("잘못된 피연산자입니다.")
-      return Error.operand
-    return Error.none
+# 다음 연산의 유형을 열거형으로 정의
+class Turn(Enum):
+    Operator = 0
+    Operand = 1
 
-#피연산자가 올바른지 검사합니다. 피연산자가 음수일 경우 '-'(음수)을 제외하고 검사
-def is_right_operand(operand):
-  index = 0
-  if operand[0] == '-':   #음수인 경우
-    index = 1
-  for i in range(index, len(operand)):
-    if not 48 <= ord(operand[index]) <= 57:
-      return False
-    index = index + 1
-  return True
-
-#콘솔 화면을 지워줍니다. 운영 체제가 유닉스 계열일 경우 'clear'를, 윈도우일 경우 'cls'를 사용
+# 콘솔 화면을 지워줍니다. 운영 체제가 유닉스 계열일 경우 'clear'를, 윈도우일 경우 'cls'를 사용
 def clear_console():
     os.system('clear' if os.name == 'posix' else 'cls')
 
-#계산기 프로그램의 메인 로직을 수행. 연산 결과를 저장하는 변수, 연산자를 저장하는 변수, 피연산자를 저장하는 변수를 선언하고, 사용자에게 연산자와 피연산자를 입력받아 계산을 수행
 def main():
+    # 변수 초기화
     result = 0
-    operand = None
     operator = Operator.addition
-    
-    clear_console()
+    error = False
+
     print("간단한 CUI 계산기")
     print("사용 가능한 연산: +, -, *(곱셈)")
-    print("종료하려면 'q'를 입력하세요.")
-
-    user_input = Operator.none  #피연산자 먼저 입력받게 함
+ 
+    turn = Turn.Operand # 이번에 어떤 종류의 입력을 받을지
+    user_input = Operator.none
 
     while True:
-      print("result = " + str(result))
+        operand = None
+        if turn == Turn.Operand:     # 이전 루프에서 연산자 혹은 잘못된 피연산자를 입력받음
+            user_input = input("피연산자를 입력하세요: ")
+        else:                       # 이전 루프에서 피연산자 혹은 잘못된 연산자를 입력받음
+            user_input = input("연산자를 입력하세요: ")
+            
+        # 이스터에그 체크 & 이스터에그 입력 즉시 바로 출력 및 종료
+        if user_input in egg:
+            print(egg[user_input])
+            exit()
 
-      if(type(user_input) == Operator):   #이전 루프에서 연산자를 입력받음 혹은 잘못된 피연산자를 입력받음
-        user_input = is_exit(input("피연산자를 입력하세요: "))
-        operand = user_input
-      else:                               #이전 루프에서 피연산자를 입력받음 혹은 잘못된 연산자를 입력받음
-        user_input = operator_kind(is_exit(input("연산자를 입력하세요: ")))
-        operator = user_input
-        if (operator == Operator.equal):
-          print(str(result))
-          user_input = operand   #등호인 경우 다음 루프에서 연산자를 입력받음
-          continue
-      
-      error = is_error(operator, result, operand)
+        # 피연산자 입력받을 차례
+        if turn == Turn.Operand:
 
-      if(error == Error.operand):
-        user_input = operator   #다음 루프에서 피연산자를 입력받음
-      elif(error == Error.operator):
-        user_input = operand    #다음 루프에서 연산자를 입력받음
-      elif(type(user_input) != Operator):
-        result = calculate(result, operator, operand)
-#크립트가 직접 실행될 때 main() 함수를 호출함. 이 코드로 인해 이 스크립트는 모듈로 불러와져 사용되는 경우에는 main() 함수가 호출되지 않음.
+            # 이전에 오류가 없었다면 입력 체크
+            if not error:
+                operand = is_right_operand(user_input)
+                
+                # 피연산자 오류 체크
+                if not operand:
+                    error = True
+                # 정상 입력이면 계산
+                else:
+                    result = calculate(result, operator, operand)
+                    
+            turn = Turn.Operator # 다음 입력은 연산자
+                  
+        else:
+            operator = is_right_operator(user_input)
+
+            if operator == Operator.equal:
+                if error:
+                    print("ERROR!")
+                else:
+                    print(result)
+                exit()
+
+            # 이전에 오류가 없었다면 입력 체크
+            if not error:
+
+                # 연산자 오류 체크
+                if not operator:
+                    error = True
+
+            turn = Turn.Operand # 다음 입력은 피연산자
+
 if __name__ == "__main__":
     main()
-'''
