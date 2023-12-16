a4882fb7e6c37a356d6dc918ef1ce2f272dbd684
eumjeongseok <133610014+eumjeongseok@users.noreply.github.com>
2023-12-16 19:45:21 +0900

    Update modified_CUI_calculator (1).py
    일부 기능 개선
    -Calculator의 팩토리얼 기능에서, 두 개 이상의 피연산자에 대해 에러 발생을 완벽하게 구현
    -테스트 케이스를 좀 더 직관적으로 수정
    
    수정을 주도해주신 김경만님, 김민혁님께 감사드립니다.

--- modified_CUI_calculator (1).py
+++ modified_CUI_calculator (1).py
@@ -20,10 +20,10 @@ class Operator(Enum):
     egg = 5
     factorial = 6
 
-# 피연산자가 올바른지 검사
+# 피연산자가 올바른지 검사합니다. 피연산자가 음수일 경우 '-'(음수)을 제외하고 검사
 def is_right_operand(operand):
     index = 0
-    if operand[0] == '-':   #음수일 경우 '-'(음수)을 제외하고 검사
+    if operand[0] == '-':   #음수인 경우
         index = 1
     for i in range(index, len(operand)):
         if not 48 <= ord(operand[index]) <= 57:
@@ -39,7 +39,7 @@ def is_right_operator(operator):
         return Operator.subtraction
     elif operator == '*' or operator == 'x' or operator == 'X':
         return Operator.multiplication
-    elif operator == '!':
+    elif operator == '!': #add for factorial
         return Operator.factorial
     elif operator == '=':
         return Operator.equal
@@ -54,20 +54,23 @@ def calculate(operand1, operator, operand2):
         return int(operand1) - int(operand2)
     elif operator == Operator.multiplication:
         return int(operand1) * int(operand2)
-    elif operator == Operator.factorial:
-        if operand1 < 0:
+    elif operator == Operator.factorial: #add for factorial
+        if operand1 < 0: #add for additional error (for operand less than 0)
+            print("[ERROR] Out Of Range")
             return -1
-        elif operand1 == 0:
+        fac_result = 1
+        multiplier = operand1
+        if operand1 == 0:
             return 1
         else:
-            fac_result = 1
             for i in range(operand1):
-               fac_result = fac_result * (i+1)
+               fac_result = fac_result * multiplier
+               multiplier = multiplier - 1
             return fac_result
     elif operator == Operator.none or operand2 == None:
         return int(operand1)
     else:
-        return False
+        return -2 #Error of Input, operator is not operator!
 
 # 다음 연산의 유형을 열거형으로 정의
 class Turn(Enum):
@@ -92,62 +95,73 @@ def main():
 
     while True:
         operand = None
-        if turn == Turn.Operand:
+        if turn == Turn.Operand:     # 이전 루프에서 연산자 혹은 잘못된 피연산자를 입력받음
             user_input = input("피연산자를 입력하세요: ")
-        else:
+        else:                       # 이전 루프에서 피연산자 혹은 잘못된 연산자를 입력받음
             user_input = input("연산자를 입력하세요: ")
             
-        # 이스터에그 입력 즉시 출력 및 종료
+        # 이스터에그 체크 & 이스터에그 입력 즉시 바로 출력 및 종료
         if user_input in egg:
             print("[EVENT] " + egg[user_input])
             exit()
 
-        # 피연산자 입력받을 차례 ========================
+        # 피연산자 입력받을 차례
         if turn == Turn.Operand:
 
-            operand = is_right_operand(user_input)
+            # 이전에 오류가 없었다면 입력 체크
+            if not error:
+                operand = is_right_operand(user_input)
                 
-            # 피연산자 오류 체크
-            if not operand:
-                error = True
-            # 정상 입력이면 계산
-            else:
-                result = calculate(result, operator, operand)    
+                # 피연산자 오류 체크
+                if not operand:
+                    error = True
+                # 정상 입력이면 계산
+                else:
+                    result = calculate(result, operator, operand)
+            
+            if user_input == '!':
+               print("[ERROR] Input Error") #if two or more individual numbers are placed before
+               result = calculate(operand, operator, user_input)
+               exit()       
             
             turn = Turn.Operator # 다음 입력은 연산자
-
-                
-        # 연산자 입력받을 차례 =========================
+                  
         else:
             operator = is_right_operator(user_input)
 
-            # 연산자 오류 체크
-            if not operator:
-                error = True
-            else:
-                if operator == Operator.equal:
-                    if error:
-                        print("[SYSTEM] ERROR!")
-                    else:
+            if operator == Operator.equal and operator != Operator.factorial:
+                if error:
+                    print("[SYSTEM] ERROR!")
+                else:
+                    print(result)
+                exit()
+            
+            if operator == Operator.factorial: #add for factorial
+                if error:
+                    if user_input == '!':
+                       result = calculate(operand, operator, 0)
+                       print("[ERROR] Input Error") #if two or more individual numbers are placed before
+                       exit() 
+                    print("[SYSTEM] ERROR!")
+                else:
+                    result = calculate(result, operator, 0)
+                    if result > 0:
                         print(result)
-                    exit()
-                elif operator == Operator.factorial:
-                    if error:
-                           print("[ERROR] Input Error")
-                    else:
-                        result = calculate(result, operator, 0)
-                        if result == -2:
-                            print("[ERROR] Out Of Range")
-                        else:
-                            print("={}".format(result))
-                    exit()
-                    
+                exit()
+
+            # 이전에 오류가 없었다면 입력 체크
+            if not error:
+
+                # 연산자 오류 체크
+                if not operator:
+                    error = True
+
             turn = Turn.Operand # 다음 입력은 피연산자
 
 #==========================================================================================================
+#Now, Let's make the Test case for project!
 
-#1. 유닛테스트
-
+#1. Unit test for some functions and Class: Class Turn, Operator, calculate
 class TestOperand(unittest.TestCase):
     def testOperand(self):
         self.assertEqual(is_right_operand('1'), '1')
@@ -163,32 +177,33 @@ class TestCalculate(unittest.TestCase):
         self.assertEqual(calculate(1, Operator.multiplication, 3), 3)
         #fill in here!
 
-#2. TDD 테스트케이스
+#2. Test Case for TDD(factorial function)
 class TestFactorial(unittest.TestCase):
     def testBasicFactorial(self):
         self.assertEqual(calculate(3, Operator.factorial, 0), 6)
         self.assertEqual(calculate(5, Operator.factorial, 0), 120)
+        
     def testMinusFactorial(self):
-        self.assertEqual(calculate(-3, Operator.factorial, 0), -2)
-        self.assertEqual(calculate(-10, Operator.factorial, 0), -2)
-        #fill in here!
+        self.assertEqual(calculate(-3, Operator.factorial, 0), -1)
+        self.assertEqual(calculate(-10, Operator.factorial, 0), -1)
+
     def testAdvanceFactorial(self):
-        self.assertEqual(calculate(3, 5, Operator.factorial), False)
-        self.assertEqual(calculate(Operator.factorial, 5, 6), False)
+        self.assertEqual(calculate(3, 5, Operator.factorial), -2)
+        self.assertEqual(calculate(4, 9, Operator.factorial), -2)
         
 
 
 #==========================================================================================================
 
 if __name__ == "__main__":
-    #main()
+    main()
     
-    #테스트를 하려면 main() 대신 이 아래의 주석 부분을 해제
-    suite = unittest.TestSuite()
+    #테스트를 하려면 main() 대신 이 아래의 주석 부분을 해제하세요. :)
+    #suite = unittest.TestSuite()
     
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestOperand))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestOperator))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCalculate))
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestFactorial))
-
-    unittest.TextTestRunner().run(suite)
+    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestOperand))
+    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestOperator))
+    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCalculate))
+    #suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestFactorial))
+    
+    #unittest.TextTestRunner().run(suite)
